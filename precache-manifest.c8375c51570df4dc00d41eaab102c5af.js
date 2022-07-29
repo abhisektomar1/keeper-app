bed9ef082eb9682359402e59219f1a02b1031baf
Abhishek <abhisektomar1@gmail.com>
2022-07-29 11:39:29 +0530

    Updates

---
+++ precache-manifest.c8375c51570df4dc00d41eaab102c5af.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "961ff5a7abcaf6948c2f85ac5b517a2a",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "33ad2cf6984f2abce024",
+    "url": "/keeper-app/static/js/2.79aacc08.chunk.js"
+  },
+  {
+    "revision": "3ab7196053b0706eda96",
+    "url": "/keeper-app/static/js/main.d2883265.chunk.js"
+  },
+  {
+    "revision": "21046adb111b2d63647b",
+    "url": "/keeper-app/static/js/runtime-main.238929eb.js"
+  }
+]);
